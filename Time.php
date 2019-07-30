bb8c3be742e2b70d0e19cde64770ffa66fb0c16d
AliveDrive <53463049+AliveDrive@users.noreply.github.com>
2019-07-30 15:54:16 -0400

    Update Time.php

--- Time.php
+++ Time.php
@@ -1,9 +1,9 @@
 <?php
-$day = 1;
-$hour = 1;
-$minute = 1;
-$second = 0;
-$second += (($day * 24 + $hour) * 60 + $minute) * 60;
-//print date(“G:i:s”, time());
-print ("$second");
+//$day = 1;
+//$hour = 1;
+//$minute = 1;
+//$second = 0;
+//$second += (($day * 24 + $hour) * 60 + $minute) * 60;
+print date(“G:i:s”, time());
+//print ("$second");
 ?>
