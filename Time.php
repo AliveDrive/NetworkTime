37f69b224af5dfce148e7b06f8733b7bc292f2ec
AliveDrive <53463049+AliveDrive@users.noreply.github.com>
2019-07-30 02:12:08 -0400

    Update Time.php

--- Time.php
+++ Time.php
@@ -1,7 +1,3 @@
 <?php
- date_default_timezone_set('US/Eastern');
- $currenttime = date("m-d-Y H:i:s");
- list($ddd,$ttt) = explode(' ',$currenttime);
- echo "$ddd/$ttt\n";
-?
->
+print date(“G.i:s”, time());
+?>
